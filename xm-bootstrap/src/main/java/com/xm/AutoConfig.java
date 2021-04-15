5c98ae238e836d42a3b41bc3222ccc8db4d7e414
qqkj <qqkj@163.com>
2021-04-16 00:52:25 +0800

    导入模块时必须选中阶段计划

--- xm-bootstrap/src/main/java/com/xm/AutoConfig.java
+++ xm-bootstrap/src/main/java/com/xm/AutoConfig.java
@@ -2,12 +2,15 @@ package com.xm;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * 要使用mdp平台功能，必须 扫码com.mdp包
  * 一些默认公共配置
  */
 @ComponentScan(basePackages={"com.mdp"})
+@EnableAsync
 @Configuration
 public class AutoConfig {
 }
