282b61e1587d0348c13d95a4f028aa44d790f7ca
陈裕财 <cyc58469@163.com>
2021-07-18 13:06:46 +0800

    生成产品项目关联关系表 生成迭代与产品关联关系表

--- xm-core/src/main/java/com/xm/core/entity/XmProductProjectLink.java
+++ xm-core/src/main/java/com/xm/core/entity/XmProductProjectLink.java
@@ -11,7 +11,7 @@ import java.util.Date;
  * 表 XM.xm_product_project_link 产品与项目的关联关系表，一般由产品经理挂接项目到产品上的所有字段名: <br>
  *	project_id,product_id,ctime,cuserid,cusername,link_status;<br>
  * 当前主键(包括多主键):<br>
- *	project_id;<br>
+ *	project_id,product_id;<br>
  */
 @ApiModel(description="产品与项目的关联关系表，一般由产品经理挂接项目到产品上")
 public class XmProductProjectLink  implements java.io.Serializable {
@@ -20,10 +20,10 @@ public class XmProductProjectLink  implements java.io.Serializable {
 	
 	@ApiModelProperty(notes="项目表中的主键,主键",allowEmptyValue=true,example="",allowableValues="")
 	String projectId;
-  	
 	
-	@ApiModelProperty(notes="产品表中的主键",allowEmptyValue=true,example="",allowableValues="")
+	@ApiModelProperty(notes="产品表中的主键,主键",allowEmptyValue=true,example="",allowableValues="")
 	String productId;
+  	
 	
 	@ApiModelProperty(notes="创建时间",allowEmptyValue=true,example="",allowableValues="")
 	Date ctime;
@@ -37,9 +37,10 @@ public class XmProductProjectLink  implements java.io.Serializable {
 	@ApiModelProperty(notes="关联状态1关联0取消关联",allowEmptyValue=true,example="",allowableValues="")
 	String linkStatus;
 
-	/**项目表中的主键**/
-	public XmProductProjectLink(String projectId) {
+	/**项目表中的主键,产品表中的主键**/
+	public XmProductProjectLink(String projectId,String productId) {
 		this.projectId = projectId;
+		this.productId = productId;
 	}
     
     /**产品与项目的关联关系表，一般由产品经理挂接项目到产品上**/
