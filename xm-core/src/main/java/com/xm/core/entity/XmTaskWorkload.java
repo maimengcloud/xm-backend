584c5ea2fa0f6fbd28e591fd68c6ab5401c34765
陈裕财 <cyc58469@163.com>
2022-07-08 00:17:09 +0800

    重构工时系统

--- xm-core/src/main/java/com/xm/core/entity/XmTaskWorkload.java
+++ xm-core/src/main/java/com/xm/core/entity/XmTaskWorkload.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 /**
  * 组织 com  顶级模块 xm 大模块 core  小模块 <br> 
  * 实体 XmTaskWorkload所有属性名: <br>
- *	"userid","员工编号","username","姓名","ctime","创建日期","taskId","业务对象主键任务编号","cuserid","创建人编号","bizDate","业务日期yyyy-MM-dd","wstatus","状态0-待确认，1-已确认，2-无效","remark","备注","ttype","任务类型-关联字典taskType","id","主键","stime","结算提交时间","sstatus","结算状态0-无需结算，1-待结算2-已提交3-已通过4-已结算","workload","工时，一个task_id可多次提交，小时","rworkload","剩余工时（同一天取最后日期更新到task表budget_workload中）","cusername","创建人姓名","projectId","归属项目","branchId","项目归属机构","ubranchId","用户归属机构","sbillId","结算单编号","sbillName","结算单名称","detailId","结算单明细表id";<br>
+ *	"userid","员工编号","username","姓名","ctime","创建日期","taskId","业务对象主键任务编号","cuserid","创建人编号","bizDate","业务日期yyyy-MM-dd","wstatus","状态0-待确认，1-已确认，2-无效","remark","备注","ttype","任务类型-关联字典taskType","id","主键","stime","结算提交时间","sstatus","结算状态0-无需结算，1-待结算2-已提交3-已通过4-已结算","workload","工时，一个task_id可多次提交，小时","rworkload","剩余工时（同一天取最后日期更新到task表budget_workload中）","cusername","创建人姓名","projectId","归属项目","branchId","项目归属机构","ubranchId","用户归属机构","sbillId","结算单编号","detailId","结算单明细表id";<br>
  * 当前主键(包括多主键):<br>
  *	id;<br>
  */
@@ -76,9 +76,6 @@ public class XmTaskWorkload  implements java.io.Serializable {
 	
 	@ApiModelProperty(notes="结算单编号",allowEmptyValue=true,example="",allowableValues="")
 	String sbillId;
-	
-	@ApiModelProperty(notes="结算单名称",allowEmptyValue=true,example="",allowableValues="")
-	String sbillName;
 	
 	@ApiModelProperty(notes="结算单明细表id",allowEmptyValue=true,example="",allowableValues="")
 	String detailId;
