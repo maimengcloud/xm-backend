4c1d63e7a9fc876642f54edb1ac281781de8f063
陈裕财 <cyc58469@163.com>
2022-07-22 13:37:33 +0800

    bug

--- xm-core/src/main/java/com/xm/core/entity/XmTestPlanCase.java
+++ xm-core/src/main/java/com/xm/core/entity/XmTestPlanCase.java
@@ -8,7 +8,7 @@ import java.util.Date;
 /**
  * 组织 com  顶级模块 xm 大模块 core  小模块 <br> 
  * 实体 XmTestPlanCase所有属性名: <br>
- *	"bugs","bug数目","execUserid","执行人","caseId","测试用例编号","ltime","更新时间","ctime","创建时间","execStatus","0-未测，1-通过，2-受阻，3-忽略，4-失败","execUsername","执行人姓名","priority","优先级","remark","执行备注","testStep","测试步骤","planId","计划编号";<br>
+ *	"bugs","bug数目","execUserid","执行人","caseId","测试用例编号","ltime","更新时间","ctime","创建时间","execStatus","0-未测，1-通过，2-受阻，3-忽略，4-失败","execUsername","执行人姓名","priority","优先级","remark","执行备注","testStep","测试步骤","planId","计划编号","projectId","项目编号";<br>
  * 当前主键(包括多主键):<br>
  *	case_id,plan_id;<br>
  */
@@ -51,6 +51,9 @@ public class XmTestPlanCase  implements java.io.Serializable {
 	
 	@ApiModelProperty(notes="测试步骤",allowEmptyValue=true,example="",allowableValues="")
 	String testStep;
+	
+	@ApiModelProperty(notes="项目编号",allowEmptyValue=true,example="",allowableValues="")
+	String projectId;
 
 	/**
 	 *测试用例编号,计划编号
