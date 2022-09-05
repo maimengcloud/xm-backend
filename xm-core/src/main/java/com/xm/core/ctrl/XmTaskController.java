7181b787a0650c4dcae469a65a9793850e532280
陈裕财 <cyc58469@163.com>
2022-09-05 16:25:29 +0800

    地里位置限制

--- xm-core/src/main/java/com/xm/core/ctrl/XmTaskController.java
+++ xm-core/src/main/java/com/xm/core/ctrl/XmTaskController.java
@@ -334,9 +334,9 @@ public class XmTaskController {
 			List<XmTask> noExecs=new ArrayList<>();
 			if(can.size()>0 && xmTaskMap.containsKey("taskState")){
 				String taskState= (String) xmTaskMap.get("taskState");
-				if( StringUtils.hasText(taskState)  && !"0".equals(taskState) && !"9".equals(taskState) ){
+				if( StringUtils.hasText(taskState)  && !"0".equals(taskState) && !"9".equals(taskState)){
 					for (XmTask task : can) {
-						if(!StringUtils.hasText(task.getExecutorUserid()) && ("0".equals(task.getTaskState())||!StringUtils.hasText(task.getTaskState()))){
+						if("0".equals(task.getNtype()) && !StringUtils.hasText(task.getExecutorUserid()) && ("0".equals(task.getTaskState())||!StringUtils.hasText(task.getTaskState()))){
 							noExecs.add(task);
 						}
 					}
