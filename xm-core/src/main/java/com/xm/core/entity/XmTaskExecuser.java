f9c467ba10124c0a6bed89ba52acdd0dae3dfbde
陈裕财 <cyc58469@163.com>
2023-01-11 01:22:11 +0800

    投标直通车

--- xm-core/src/main/java/com/xm/core/entity/XmTaskExecuser.java
+++ xm-core/src/main/java/com/xm/core/entity/XmTaskExecuser.java
@@ -1,21 +1,20 @@
 package  com.xm.core.entity;
 
+import lombok.Data;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-
-import java.math.BigDecimal;
 import java.util.Date;
+import java.math.BigDecimal;
 
 /**
  * 组织 com  顶级模块 xm 大模块 core  小模块 <br> 
  * 实体 XmTaskExecuser所有属性名: <br>
- *	createTime,taskId,userid,startTime,endTime,status,remarks,createUserid,createUsername,username,matchScore,quoteWeekday,quoteAmount,quoteTime,projectId,phaseId,skillRemark,quoteWorkload,quoteStartTime,quoteEndTime,branchId,phaseName,taskName,distUserid,distUsername,execUserBranchId,shareKey,sfeeRate,sfee;<br>
- * 表 xm_task_execuser xm_task_execuser的所有字段名: <br>
- *	create_time,task_id,userid,start_time,end_time,status,remarks,create_userid,create_username,username,match_score,quote_weekday,quote_amount,quote_time,project_id,phase_id,skill_remark,quote_workload,quote_start_time,quote_end_time,branch_id,phase_name,task_name,dist_userid,dist_username,exec_user_branch_id,share_key,sfee_rate,sfee;<br>
+ *	"createTime","创建时间","taskId","任务id","userid","执行人id","startTime","加入时间","endTime","离开时间","status","执行人状态0候选排队中1执行任务中7放弃任务8黑名单","remarks","备注","createUserid","创建人","createUsername","创建人姓名","username","执行人姓名","matchScore","任务能力匹配分数","quoteWeekday","报价天数，不包括周六日","quoteAmount","报价金额","quoteTime","报价时间","projectId","项目编号","phaseId","阶段计划编号","skillRemark","技能说明","quoteWorkload","报价工作量单位人时","quoteStartTime","报价-开始工作日期","quoteEndTime","报价-结束工作日期","branchId","项目所属机构","phaseName","阶段计划名称","taskName","任务名称","distUserid","推荐人编号","distUsername","推荐人姓名","execUserBranchId","执行人归属公司","shareKey","分享码","sfeeRate","服务费率","sfee","众包服务费","provinceId","省编号","provinceName","省名称","cityId","城市编号","cityName","城市名称","areaId","区县编号","areaName","区县名称","gradeId","能力等级编号","guardId","保障等级编号","ilvlId","会员等级编号","creditId","信用等级编号","ctotalBids","投标总数","srvTimes","服务总次数","cmonthExp","本月获得经验值","cmonthBids","本月投标数","bidDirect","是否开启投标直通车";<br>
  * 当前主键(包括多主键):<br>
  *	task_id,userid;<br>
  */
-@ApiModel(description="xm_task_execuser")
+ @Data
+@ApiModel(description="任务候选人、执行人表")
 public class XmTaskExecuser  implements java.io.Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -107,365 +106,64 @@ public class XmTaskExecuser  implements java.io.Serializable {
 	
 	@ApiModelProperty(notes="众包服务费",allowEmptyValue=true,example="",allowableValues="")
 	BigDecimal sfee;
+	
+	@ApiModelProperty(notes="省编号",allowEmptyValue=true,example="",allowableValues="")
+	String provinceId;
+	
+	@ApiModelProperty(notes="省名称",allowEmptyValue=true,example="",allowableValues="")
+	String provinceName;
+	
+	@ApiModelProperty(notes="城市编号",allowEmptyValue=true,example="",allowableValues="")
+	String cityId;
+	
+	@ApiModelProperty(notes="城市名称",allowEmptyValue=true,example="",allowableValues="")
+	String cityName;
+	
+	@ApiModelProperty(notes="区县编号",allowEmptyValue=true,example="",allowableValues="")
+	String areaId;
+	
+	@ApiModelProperty(notes="区县名称",allowEmptyValue=true,example="",allowableValues="")
+	String areaName;
+	
+	@ApiModelProperty(notes="能力等级编号",allowEmptyValue=true,example="",allowableValues="")
+	String gradeId;
+	
+	@ApiModelProperty(notes="保障等级编号",allowEmptyValue=true,example="",allowableValues="")
+	String guardId;
+	
+	@ApiModelProperty(notes="会员等级编号",allowEmptyValue=true,example="",allowableValues="")
+	String ilvlId;
+	
+	@ApiModelProperty(notes="信用等级编号",allowEmptyValue=true,example="",allowableValues="")
+	String creditId;
+	
+	@ApiModelProperty(notes="投标总数",allowEmptyValue=true,example="",allowableValues="")
+	Integer ctotalBids;
+	
+	@ApiModelProperty(notes="服务总次数",allowEmptyValue=true,example="",allowableValues="")
+	Integer srvTimes;
+	
+	@ApiModelProperty(notes="本月获得经验值",allowEmptyValue=true,example="",allowableValues="")
+	BigDecimal cmonthExp;
+	
+	@ApiModelProperty(notes="本月投标数",allowEmptyValue=true,example="",allowableValues="")
+	Integer cmonthBids;
+	
+	@ApiModelProperty(notes="是否开启投标直通车",allowEmptyValue=true,example="",allowableValues="")
+	String bidDirect;
 
-	/**任务id,执行人id**/
+	/**
+	 *任务id,执行人id
+	 **/
 	public XmTaskExecuser(String taskId,String userid) {
 		this.taskId = taskId;
 		this.userid = userid;
 	}
     
-    /**xm_task_execuser**/
+    /**
+     * 任务候选人、执行人表
+     **/
 	public XmTaskExecuser() {
-	}
-	
-	/**
-	 * 创建时间
-	 **/
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-	/**
-	 * 任务id
-	 **/
-	public void setTaskId(String taskId) {
-		this.taskId = taskId;
-	}
-	/**
-	 * 执行人id
-	 **/
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-	/**
-	 * 加入时间
-	 **/
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}
-	/**
-	 * 离开时间
-	 **/
-	public void setEndTime(Date endTime) {
-		this.endTime = endTime;
-	}
-	/**
-	 * 执行人状态0候选排队中1执行任务中7放弃任务8黑名单
-	 **/
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	/**
-	 * 备注
-	 **/
-	public void setRemarks(String remarks) {
-		this.remarks = remarks;
-	}
-	/**
-	 * 创建人
-	 **/
-	public void setCreateUserid(String createUserid) {
-		this.createUserid = createUserid;
-	}
-	/**
-	 * 创建人姓名
-	 **/
-	public void setCreateUsername(String createUsername) {
-		this.createUsername = createUsername;
-	}
-	/**
-	 * 执行人姓名
-	 **/
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	/**
-	 * 任务能力匹配分数
-	 **/
-	public void setMatchScore(BigDecimal matchScore) {
-		this.matchScore = matchScore;
-	}
-	/**
-	 * 报价天数，不包括周六日
-	 **/
-	public void setQuoteWeekday(BigDecimal quoteWeekday) {
-		this.quoteWeekday = quoteWeekday;
-	}
-	/**
-	 * 报价金额
-	 **/
-	public void setQuoteAmount(BigDecimal quoteAmount) {
-		this.quoteAmount = quoteAmount;
-	}
-	/**
-	 * 报价时间
-	 **/
-	public void setQuoteTime(Date quoteTime) {
-		this.quoteTime = quoteTime;
-	}
-	/**
-	 * 项目编号
-	 **/
-	public void setProjectId(String projectId) {
-		this.projectId = projectId;
-	}
-	/**
-	 * 阶段计划编号
-	 **/
-	public void setPhaseId(String phaseId) {
-		this.phaseId = phaseId;
-	}
-	/**
-	 * 技能说明
-	 **/
-	public void setSkillRemark(String skillRemark) {
-		this.skillRemark = skillRemark;
-	}
-	/**
-	 * 报价工作量单位人时
-	 **/
-	public void setQuoteWorkload(BigDecimal quoteWorkload) {
-		this.quoteWorkload = quoteWorkload;
-	}
-	/**
-	 * 报价-开始工作日期
-	 **/
-	public void setQuoteStartTime(Date quoteStartTime) {
-		this.quoteStartTime = quoteStartTime;
-	}
-	/**
-	 * 报价-结束工作日期
-	 **/
-	public void setQuoteEndTime(Date quoteEndTime) {
-		this.quoteEndTime = quoteEndTime;
-	}
-	/**
-	 * 项目所属机构
-	 **/
-	public void setBranchId(String branchId) {
-		this.branchId = branchId;
-	}
-	/**
-	 * 阶段计划名称
-	 **/
-	public void setPhaseName(String phaseName) {
-		this.phaseName = phaseName;
-	}
-	/**
-	 * 任务名称
-	 **/
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
-	/**
-	 * 推荐人编号
-	 **/
-	public void setDistUserid(String distUserid) {
-		this.distUserid = distUserid;
-	}
-	/**
-	 * 推荐人姓名
-	 **/
-	public void setDistUsername(String distUsername) {
-		this.distUsername = distUsername;
-	}
-	/**
-	 * 执行人归属公司
-	 **/
-	public void setExecUserBranchId(String execUserBranchId) {
-		this.execUserBranchId = execUserBranchId;
-	}
-	/**
-	 * 分享码
-	 **/
-	public void setShareKey(String shareKey) {
-		this.shareKey = shareKey;
-	}
-	/**
-	 * 服务费率
-	 **/
-	public void setSfeeRate(Integer sfeeRate) {
-		this.sfeeRate = sfeeRate;
-	}
-	/**
-	 * 众包服务费
-	 **/
-	public void setSfee(BigDecimal sfee) {
-		this.sfee = sfee;
-	}
-	
-	/**
-	 * 创建时间
-	 **/
-	public Date getCreateTime() {
-		return this.createTime;
-	}
-	/**
-	 * 任务id
-	 **/
-	public String getTaskId() {
-		return this.taskId;
-	}
-	/**
-	 * 执行人id
-	 **/
-	public String getUserid() {
-		return this.userid;
-	}
-	/**
-	 * 加入时间
-	 **/
-	public Date getStartTime() {
-		return this.startTime;
-	}
-	/**
-	 * 离开时间
-	 **/
-	public Date getEndTime() {
-		return this.endTime;
-	}
-	/**
-	 * 执行人状态0候选排队中1执行任务中7放弃任务8黑名单
-	 **/
-	public String getStatus() {
-		return this.status;
-	}
-	/**
-	 * 备注
-	 **/
-	public String getRemarks() {
-		return this.remarks;
-	}
-	/**
-	 * 创建人
-	 **/
-	public String getCreateUserid() {
-		return this.createUserid;
-	}
-	/**
-	 * 创建人姓名
-	 **/
-	public String getCreateUsername() {
-		return this.createUsername;
-	}
-	/**
-	 * 执行人姓名
-	 **/
-	public String getUsername() {
-		return this.username;
-	}
-	/**
-	 * 任务能力匹配分数
-	 **/
-	public BigDecimal getMatchScore() {
-		return this.matchScore;
-	}
-	/**
-	 * 报价天数，不包括周六日
-	 **/
-	public BigDecimal getQuoteWeekday() {
-		return this.quoteWeekday;
-	}
-	/**
-	 * 报价金额
-	 **/
-	public BigDecimal getQuoteAmount() {
-		return this.quoteAmount;
-	}
-	/**
-	 * 报价时间
-	 **/
-	public Date getQuoteTime() {
-		return this.quoteTime;
-	}
-	/**
-	 * 项目编号
-	 **/
-	public String getProjectId() {
-		return this.projectId;
-	}
-	/**
-	 * 阶段计划编号
-	 **/
-	public String getPhaseId() {
-		return this.phaseId;
-	}
-	/**
-	 * 技能说明
-	 **/
-	public String getSkillRemark() {
-		return this.skillRemark;
-	}
-	/**
-	 * 报价工作量单位人时
-	 **/
-	public BigDecimal getQuoteWorkload() {
-		return this.quoteWorkload;
-	}
-	/**
-	 * 报价-开始工作日期
-	 **/
-	public Date getQuoteStartTime() {
-		return this.quoteStartTime;
-	}
-	/**
-	 * 报价-结束工作日期
-	 **/
-	public Date getQuoteEndTime() {
-		return this.quoteEndTime;
-	}
-	/**
-	 * 项目所属机构
-	 **/
-	public String getBranchId() {
-		return this.branchId;
-	}
-	/**
-	 * 阶段计划名称
-	 **/
-	public String getPhaseName() {
-		return this.phaseName;
-	}
-	/**
-	 * 任务名称
-	 **/
-	public String getTaskName() {
-		return this.taskName;
-	}
-	/**
-	 * 推荐人编号
-	 **/
-	public String getDistUserid() {
-		return this.distUserid;
-	}
-	/**
-	 * 推荐人姓名
-	 **/
-	public String getDistUsername() {
-		return this.distUsername;
-	}
-	/**
-	 * 执行人归属公司
-	 **/
-	public String getExecUserBranchId() {
-		return this.execUserBranchId;
-	}
-	/**
-	 * 分享码
-	 **/
-	public String getShareKey() {
-		return this.shareKey;
-	}
-	/**
-	 * 服务费率
-	 **/
-	public Integer getSfeeRate() {
-		return this.sfeeRate;
-	}
-	/**
-	 * 众包服务费
-	 **/
-	public BigDecimal getSfee() {
-		return this.sfee;
 	}
 
 }
