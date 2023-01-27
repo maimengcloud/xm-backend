fae79ffb06f42e192b5fb64b43c86479b0efbe9a
陈裕财 <cyc58469@163.com>
2023-01-27 14:37:32 +0800

    缺陷回归分布

--- xm-core/src/main/java/com/xm/core/entity/XmIteration.java
+++ xm-core/src/main/java/com/xm/core/entity/XmIteration.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 /**
  * 组织 com  顶级模块 xm 大模块 core  小模块 <br> 
  * 实体 XmIteration所有属性名: <br>
- *	"id","迭代编码","branchId","机构编号","iterationName","迭代名称","startTime","开始时间","endTime","结束时间","onlineTime","上线时间","pid","上级迭代-作废，不以树状结构","adminUserid","负责人","adminUsername","负责人姓名","ctime","创建时间","budgetCost","预算成本","budgetWorkload","预算工作量","seqNo","顺序号","istatus","迭代状态0未结束1已结束","cuserid","创建人编号","cusername","创建人人姓名","remark","备注","iphase","迭代阶段:0未开始,1需求评审,2计划会,3研发中,4测试中,5迭代上线,6已完成7已关闭","isTpl","是否为模板","productId","产品编号";<br>
+ *	"id","迭代编码","branchId","机构编号","iterationName","迭代名称","startTime","开始时间","endTime","结束时间","onlineTime","上线时间","pid","上级迭代-作废，不以树状结构","adminUserid","负责人","adminUsername","负责人姓名","ctime","创建时间","budgetCost","预算成本","budgetWorkload","预算工作量","seqNo","顺序号","istatus","迭代状态0未结束1已结束","cuserid","创建人编号","cusername","创建人人姓名","remark","备注","iphase","迭代阶段:0未开始,1需求评审,2计划会,3研发中,4测试中,5迭代上线,6已完成7已关闭","isTpl","是否为模板","productId","产品编号","productName","产品名称";<br>
  * 当前主键(包括多主键):<br>
  *	id;<br>
  */
@@ -79,6 +79,9 @@ public class XmIteration  implements java.io.Serializable {
 	
 	@ApiModelProperty(notes="产品编号",allowEmptyValue=true,example="",allowableValues="")
 	String productId;
+	
+	@ApiModelProperty(notes="产品名称",allowEmptyValue=true,example="",allowableValues="")
+	String productName;
 
 	/**
 	 *迭代编码
