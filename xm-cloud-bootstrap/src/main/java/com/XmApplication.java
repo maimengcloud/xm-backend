478a39af53505f2420fc8dc47893f2a141f975cc
陈裕财 <cyc58469@163.com>
2024-01-30 22:32:45 +0800

    解决候选人更新不成功问题

--- xm-cloud-bootstrap/src/main/java/com/XmApplication.java
+++ xm-cloud-bootstrap/src/main/java/com/XmApplication.java
@@ -1,8 +1,10 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@SpringCloudApplication
 @EnableRedisHttpSession
 public class XmApplication  {
 	
