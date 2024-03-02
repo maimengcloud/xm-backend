139dac133581b5be9a99ab7a85c1c3e7964fa60e
陈裕财 <cyc58469@163.com>
2024-03-02 17:51:27 +0800

    优化启动-xm-bootstrap XmApplication增加注解 @SpringBootApplication

--- xm-bootstrap/src/main/java/com/XmApplication.java
+++ xm-bootstrap/src/main/java/com/XmApplication.java
@@ -1,8 +1,9 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
+@SpringBootApplication
 @EnableRedisHttpSession
 public class XmApplication  {
 	
